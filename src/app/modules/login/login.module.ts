366df98064ebde749b4d11a9c9e5cddbb6d371b8
Jagan Aravind Ravindranath <JaganAravind@srinsofttech.lan>
2020-02-04 05:53:29 -0500

    Fourth Commit

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { CheckboxModule, WavesModule, ButtonsModule, InputsModule, IconsModule, CardsModule } from 'angular-bootstrap-md'
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
 
@@ -8,7 +9,15 @@ import { LoginComponent } from './components/login/login.component';
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    WavesModule,
+    ButtonsModule,
+    InputsModule,
+    IconsModule,
+    CardsModule
   ]
 })
 export class LoginModule { }
